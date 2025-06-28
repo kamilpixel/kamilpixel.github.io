21b77562bf7d73bdbb5fca8055a47997c2bc867f
Kamil <48324735+kamilpixel@users.noreply.github.com>
2025-06-28 21:27:46 +0800

    Fix page refresh issue

--- assets/useProfileImage-CimdQxCh.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-4IAnC6sS.js";function f(){return{profileImage:e("/assets/profile.jpg")}}export{f as u};
---
+++ assets/useProfileImage-Im2yZsF7.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-GnscuxVV.js";function f(){return{profileImage:e("/assets/profile.jpg")}}export{f as u};
