727b8ebd58a48f45af07295890dd0368ac186216
Kamil <48324735+kamilpixel@users.noreply.github.com>
2025-06-28 22:44:16 +0800

    Fix subpage refresh. added basic meta

--- assets/useProfileImage-D-xtu77k.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-CDxgHNv2.js";function f(){return{profileImage:e("/assets/profile.jpg")}}export{f as u};
---
+++ assets/useProfileImage-DDd20xJs.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-lk8gBRS_.js";function f(){return{profileImage:e("/assets/profile.jpg")}}export{f as u};
